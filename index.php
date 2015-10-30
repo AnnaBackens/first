f32e1e202189808c370e7e9e542677d927ed3963
anna <anna.backens@gmail.com>
2015-10-29 21:33:49 -0400

    my first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+ <?php
+$First = "variable";
+ ?>
+ <!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="style.css">
+<title>Anna's page</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+<p><?php
+echo $First;
+?></p>
+</body>
+</html> 
